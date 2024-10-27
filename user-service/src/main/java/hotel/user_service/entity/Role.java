03f7635a55ecb6038ed8056f7d2141161a9be252
Dan Onofrei <dandrei.onofrei@gmail.com>
2024-10-27 13:40:49 +0200

    create user_role, hotel_user entities and tables

---
+++ user-service/src/main/java/hotel/user_service/entity/Role.java
@@ -0,0 +1,23 @@
+package hotel.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String roleName;
+}
---
+++ user-service/src/main/java/hotel/user_service/entity/User.java
@@ -0,0 +1,38 @@
+package hotel.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hotel_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String phoneNumber;
+
+    @OneToOne
+    @JoinColumn(name = "role_id")
+    private Role userRole;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
