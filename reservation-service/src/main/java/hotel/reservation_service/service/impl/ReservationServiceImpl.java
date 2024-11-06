bc24d1cf8d33583a36680b07dca4de3ce71bf61f
Dan Onofrei <dandrei.onofrei@gmail.com>
2024-11-06 07:38:57 +0200

    reformat ResponseApi methods

--- reservation-service/src/main/java/hotel/reservation_service/service/impl/ReservationServiceImpl.java
+++ reservation-service/src/main/java/hotel/reservation_service/service/impl/ReservationServiceImpl.java
@@ -26,18 +26,6 @@ public class ReservationServiceImpl implements ReservationService {
     @Override
     public APIResponseDto saveReservation(Long userId, Long roomId) {
 
-        UserDto userDto = webClient.get()
-                .uri("http://localhost:8080/api/users/" + userId)
-                .retrieve()
-                .bodyToMono(UserDto.class)
-                .block();
-
-        RoomDto roomDto = webClient.get()
-                .uri("http://localhost:8081/api/rooms/" + roomId)
-                .retrieve()
-                .bodyToMono(RoomDto.class)
-                .block();
-
         // Create and save a new Reservation
         Reservation reservation = new Reservation();
         reservation.setUserId(userId);
@@ -54,10 +42,15 @@ public class ReservationServiceImpl implements ReservationService {
         reservationDto.setReservationStatus(String.valueOf(savedReservation.getReservationStatus()));
         reservationDto.setCreatedAt(savedReservation.getCreatedAt());
 
+        UserDto userDto = getUserResponseApi(userId);
+        RoomDto roomDto = getRoomResponseApi(roomId);
+
         APIResponseDto apiResponseDto = new APIResponseDto();
+
         apiResponseDto.setUser(userDto);
         apiResponseDto.setRoom(roomDto);
         apiResponseDto.setReservation(reservationDto);
+
         return apiResponseDto;
     }
 
@@ -75,38 +68,62 @@ public class ReservationServiceImpl implements ReservationService {
         Reservation updatedReservation = reservationRepository.save(reservation);
         ReservationDto reservationDto = ReservationMapper.mapToReservationDto(updatedReservation);
 
-        UserDto userDto = webClient.get()
-        .uri("http://localhost:8080/api/users/" + userId)
-        .retrieve()
-        .bodyToMono(UserDto.class)
-        .block();
+        UserDto userDto = getUserResponseApi(userId);
 
         //set existing hotel services of the reservation to the APIResponseDto
         List<HotelServiceDto> userHotelServices = new ArrayList<>();
-        for(int i=0;i<reservationServices.size();i++){
 
-                HotelServiceDto serviceDto = webClient.get()
-                .uri("http://localhost:8082/api/services/" + reservationServices.get(i))
-                .retrieve()
-                .bodyToMono(HotelServiceDto.class)
-                .block();
+        for (int i = 0; i < reservationServices.size(); i++) {
 
+            HotelServiceDto serviceDto = getHotelServiceResponseApi(reservationServices.get(i));
             userHotelServices.add(serviceDto);
         }
 
-        RoomDto roomDto =  webClient.get()
-                .uri("http://localhost:8081/api/rooms/" + reservationDto.getRoomId())
-                .retrieve()
-                .bodyToMono(RoomDto.class)
-                .block();
+        RoomDto roomDto = getRoomResponseApi(reservationDto.getRoomId());
 
         APIResponseDto apiResponseDto = new APIResponseDto();
+
         apiResponseDto.setReservation(reservationDto);
         apiResponseDto.setUser(userDto);
         apiResponseDto.setRoom(roomDto);
+
         apiResponseDto.setHotelServices(userHotelServices);
 
         return apiResponseDto;
 
     }
+
+    private UserDto getUserResponseApi(Long userId) {
+
+        UserDto userDto = webClient.get()
+                .uri("http://localhost:8080/api/users/" + userId)
+                .retrieve()
+                .bodyToMono(UserDto.class)
+                .block();
+
+        return userDto;
+    }
+
+    private RoomDto getRoomResponseApi(Long roomId) {
+
+        RoomDto roomDto = webClient.get()
+                .uri("http://localhost:8081/api/rooms/" + roomId)
+                .retrieve()
+                .bodyToMono(RoomDto.class)
+                .block();
+
+        return roomDto;
+    }
+
+    private HotelServiceDto getHotelServiceResponseApi(Long serviceId) {
+
+        HotelServiceDto serviceDto = webClient.get()
+                .uri("http://localhost:8082/api/services/" + serviceId)
+                .retrieve()
+                .bodyToMono(HotelServiceDto.class)
+                .block();
+
+        return serviceDto;
+    }
+
 }
