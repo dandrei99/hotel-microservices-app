5bff1959259af680ce474c073d84b0818b7bec76
Dan Onofrei <dandrei.onofrei@gmail.com>
2024-11-11 17:52:22 +0200

    HOME page: 2nd about, footer sections added

--- hotel-frontend/src/pages/Index.js
+++ hotel-frontend/src/pages/Index.js
@@ -13,6 +13,13 @@ import slide4 from '../assets/images/slide4.jpg';
 import top from '../assets/images/top.jpg';
 import bottom from '../assets/images/bottom.jpg';
 
+//2nd about images
+import room1 from '../assets/images/room1.jpg';
+import room2 from '../assets/images/room2.jpg';
+import room3 from '../assets/images/room3.jpg';
+import room4 from '../assets/images/room4.jpg';
+import room5 from '../assets/images/room5.jpg';
+
 const Index = () => {
     useEffect(() => {
         // Ensure jQuery and Owl Carousel are loaded
@@ -257,7 +264,7 @@ const Index = () => {
                 </div>
             </section>
 
-            {/*1st ABOUT*/}
+            {/*1st ABOUT section*/}
             <section className="w3l-about py-5">
                 <div className="container py-sm-4">
                     <div className="row">
@@ -280,6 +287,158 @@ const Index = () => {
                             <img src={top} alt="Top Image" className="img-fluid img-border mt-4"/>
                             <img src={bottom} alt="Bottom Image" className="img-fluid position-absolute img-position"
                             />
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/*2nd ABOUT section*/}
+            <div className="best-rooms py-5">
+                <div className="container py-lg-5 py-sm-4">
+                    <h3 className="title-big text-center">Best Rooms</h3>
+                    <div className="ban-content-inf row py-lg-3">
+                        <div className="maghny-gd-1 col-lg-6">
+                            <div className="maghny-grid">
+                                <figure className="effect-lily">
+                                    <img src={room1} className="img-fluid" alt="Room1"/>
+                                    <figcaption className="w3set-hny">
+                                        <div>
+                                            <h4 className="top-text">Luxury Hotel and Best Resort
+                                                <span>Peaceful Place to stay</span></h4>
+                                            <p>From 20$ </p>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                                <div className="room-info">
+                                    <h3 className="room-title"><a href="room-single.html">Luxury Hotel</a></h3>
+                                    <ul className="mb-3">
+                                        <li><span className="fa fa-users"></span> 2 Guests</li>
+                                        <li><span className="fa fa-bed"></span> Double bed</li>
+                                        <li><span className="fa fa-bed"></span> 15sqft</li>
+                                    </ul>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A recusandae, illum
+                                        sequi numquam
+                                        tempora voluptates?</p>
+                                    <a href="#book" className="btn btn-style btn-primary mt-sm-4 mt-3">Book Now</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="maghny-gd-1 col-lg-6 mt-lg-0 mt-4">
+                            <div className="row">
+                                <div className="maghny-gd-1 col-6">
+                                    <div className="maghny-grid">
+                                        <figure className="effect-lily border-radius">
+                                            <img src={room2} className="img-fluid" alt="Room2"/>
+                                            <figcaption>
+                                                <div>
+                                                    <h4>Family Rooms <span> Resort</span></h4>
+                                                    <p>From 20$ </p>
+                                                </div>
+
+                                            </figcaption>
+                                        </figure>
+                                    </div>
+                                </div>
+                                <div className="maghny-gd-1 col-6">
+                                    <div className="maghny-grid">
+                                        <figure className="effect-lily border-radius">
+                                            <img src={room3} className="img-fluid" alt="Room3"/>
+                                            <figcaption>
+                                                <div>
+                                                    <h4>Double Rooms <span> Resort</span></h4>
+                                                    <p>From 20$ </p>
+                                                </div>
+
+                                            </figcaption>
+                                        </figure>
+                                    </div>
+                                </div>
+                                <div className="maghny-gd-1 col-6 mt-4">
+                                    <div className="maghny-grid">
+                                        <figure className="effect-lily border-radius">
+                                            <img src={room4} className="img-fluid" alt="Room4"/>
+                                            <figcaption>
+                                                <div>
+                                                    <h4>Luxury Rooms <span> Resort</span></h4>
+                                                    <p>From 20$ </p>
+                                                </div>
+
+                                            </figcaption>
+                                        </figure>
+                                    </div>
+                                </div>
+                                <div className="maghny-gd-1 col-6 mt-4">
+                                    <div className="maghny-grid">
+                                        <figure className="effect-lily border-radius">
+                                            <img src={room5} className="img-fluid" alt="Room5"/>
+                                            <figcaption>
+                                                <div>
+                                                    <h4>Resort Rooms <span> Resort</span></h4>
+                                                    <p>From 20$ </p>
+                                                </div>
+
+                                            </figcaption>
+                                        </figure>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/*Footer*/}
+            <section className="w3l-footer-29-main">
+                <div className="footer-29 py-5">
+                    <div className="container py-lg-4">
+                        <div className="row footer-top-29">
+                            <div className="col-lg-3 col-md-6 col-sm-8 footer-list-29 footer-1">
+                                <h6 className="footer-title-29">Contact Us</h6>
+                                <ul>
+                                    <li>
+                                        <p><span className="fa fa-map-marker"></span> Luxury hotel, #32841 block,
+                                            #221DRS Rental & Paid rooms
+                                            business, UK.</p>
+                                    </li>
+                                    <li><a href="tel:+7-800-999-800"><span
+                                        className="fa fa-phone"></span> +(21)-255-999-8888</a></li>
+                                    <li><a href="mailto:hotels@mail.com" className="mail"><span
+                                        className="fa fa-envelope-open-o"></span>
+                                        hotels@mail.com</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-2 col-md-6 col-sm-4 footer-list-29 footer-2 mt-sm-0 mt-5">
+
+                                <ul>
+                                    <h6 className="footer-title-29">Useful Links</h6>
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="about.html">About hotels</a></li>
+                                    <li><a href="blog.html"> Blog posts</a></li>
+                                    <li><a href="contact.html">Contact us</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-3 col-md-6 col-sm-5 footer-list-29 footer-3 mt-lg-0 mt-5">
+                                <h6 className="footer-title-29">Latest from blog</h6>
+                                <div className="footer-post mb-4">
+                                    <a href="blog-single.html">Work Passionately</a>
+                                    <p className="small"><span className="fa fa-clock-o"></span> March 9, 2020</p>
+                                </div>
+                                <div className="footer-post">
+                                    <a href="blog-single.html">Work Passionately without any hesitation</a>
+                                    <p className="small"><span className="fa fa-clock-o"></span> March 9, 2020</p>
+                                </div>
+
+                            </div>
+                            <div className="col-lg-4 col-md-6 col-sm-7 footer-list-29 footer-4 mt-lg-0 mt-5">
+                                <h6 className="footer-title-29">Newsletter </h6>
+                                <p>Enter your email and receive the latest news from us.
+                                    We'll never share your email address</p>
+
+                                <form action="#" className="subscribe" method="post">
+                                    <input type="email" name="email" placeholder="Your Email Address" required=""/>
+                                    <button><span className="fa fa-envelope-o"></span></button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
