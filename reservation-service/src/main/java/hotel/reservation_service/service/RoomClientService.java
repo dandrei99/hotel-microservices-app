282503cf00cee4d173c1b30986a67c3195e9e37b
Dan Onofrei <dandrei.onofrei@gmail.com>
2025-05-26 14:05:39 +0300

    Resilience4j for Room-Service in "getReservation" 2

---
+++ reservation-service/src/main/java/hotel/reservation_service/service/RoomClientService.java
@@ -0,0 +1,42 @@
+package hotel.reservation_service.service;
+
+import hotel.reservation_service.dto.RoomDto;
+import hotel.reservation_service.service.impl.ReservationServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class RoomClientService {
+    private final WebClient.Builder webClientBuilder;
+    private static final Logger log = LoggerFactory.getLogger(RoomClientService.class);
+
+    public RoomClientService(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    @CircuitBreaker(name = "roomService", fallbackMethod = "fallbackRoom")
+    public RoomDto getRoom(Long roomId, String token) {
+        WebClient securedWebClient = webClientBuilder.build();
+        RoomDto roomDto =  securedWebClient.get()
+                .uri("http://localhost:9191/api/rooms/" + roomId)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .retrieve()
+                .bodyToMono(RoomDto.class)
+                .block();
+        return roomDto;
+    }
+
+
+    public RoomDto fallbackRoom(Long roomId, String token, Throwable throwable) {
+        log.error("Fallback triggered for RoomService. Reason: {}", throwable.getMessage());
+        RoomDto fallbackDto = new RoomDto();
+        fallbackDto.setRoomId(roomId);
+        fallbackDto.setRoomType("Unavailable");
+        fallbackDto.setDescription("Room info not available");
+        return fallbackDto;
+    }
+}
